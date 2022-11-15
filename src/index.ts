e5480a3d45b448c7787c447c0f21b79077d5971a
Daichi Sakai <da127sk@gmail.com>
2022-11-15 06:29:08 -0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata"
+
+import { createSchema, createYoga } from 'graphql-yoga'
+import { createServer } from 'node:http'
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'Bye'
+      }
+    }
+  })
+})
+
+const server = createServer(yoga)
+
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
